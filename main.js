33c1f96e32a842771becb5278ec877a59de3f809
ZakiHidayat <zakihidayat260@gmail.com>
2022-11-23 14:18:24 +0700

    Update

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const alert = (message, type) => {
   wrapper.innerHTML = [
     `<div class="alert alert-${type} alert-dismissible" role="alert">`,
     `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '   <button type="button" id="ini_tutup" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
     '</div>'
   ].join('')
 
@@ -16,5 +16,11 @@ const alertTrigger = document.getElementById('liveAlertBtn')
 if (alertTrigger) {
   alertTrigger.addEventListener('click', () => {
     alert('Nice, you triggered this alert message!', 'success')
-  })
-}
+    setTimeout(function(){
+      document.getElementById('ini_tutup').click();}, 6000);
+    })
+  }
+
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
